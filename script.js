1eb2442291afb8776bf03214b8b62f7284d26bda
Raynelle <raynelle.k.forrester@student.uncommoncollegiate.org>
2019-05-08 21:48:30 +0000

    work

---
+++ script.js
@@ -0,0 +1,10 @@
+/*global $*/
+$.ajax({
+    url:"https://www.googleapis.com/books/v1/volumes?q=harry%20potter",
+    method: "GET",
+    success:function(response){
+        $("body").append("<img class=\"bimg\" scr="+ response.items[0].volumeInfo.imageLink.smallThumbnail +">");
+    },
+    
+});
+
