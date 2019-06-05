d90026c683aab6a1b7de2c0afc8e94ff18cd43e2
Raynelle <raynelle.k.forrester@student.uncommoncollegiate.org>
2019-06-05 02:33:55 +0000

    final touches

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ $.ajax({
     url:"https://www.googleapis.com/books/v1/volumes?q=harry%20potter",
     method: "GET",
     success:function(response){
-        $("#im").append("<a href=\" \"><img id=\"first\" class=\"bimg\" onclick="changeItem()" src=" + response.items[0].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"second\" class=\"bimg\" src=" + response.items[1].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"third\" class=\"bimg\" src=" + response.items[2].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"fourth\" class=\"bimg\" src=" + response.items[3].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"fifth\" class=\"bimg\" src=" + response.items[4].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"sixth\" class=\"bimg\" src=" + response.items[5].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"seventh\" class=\"bimg\" src=" + response.items[6].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"eighth\" class=\"bimg\" src=" + response.items[7].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"ninth\" class=\"bimg\" src=" + response.items[8].volumeInfo.imageLinks.smallThumbnail + "></a>" );
-        $("#im").append("<a href=\" \"><img id=\"tenth\" class=\"bimg\" src=" + response.items[9].volumeInfo.imageLinks.smallThumbnail + "></a>" );
+        $("#im").append("<a href=\" \"><img id=\"first\" class=\"bimg\" src=" + response.items[0].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[0].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[0]            .volumeInfo.authors +"</strong><br />" + response          .items[0].volumeInfo.publishedDate + "<br />"+ response.items[0].volumeInfo.description +"</p>" );
+        
+        $("#im").append("<a href=\" \"><img id=\"second\" class=\"bimg\" src=" + response.items[1].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[1].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[1].volumeInfo.authors +"</strong><br />" + response.items[1].volumeInfo.publishedDate + "<br />"+ response.items[1].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"third\" class=\"bimg\" src=" + response.items[2].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[2].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[2].volumeInfo.authors +"</strong><br />" + response.items[2].volumeInfo.publishedDate + "<br />"+ response.items[2].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"fourth\" class=\"bimg\" src=" + response.items[3].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[3].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[3].volumeInfo.authors +"</strong><br />" + response.items[3].volumeInfo.publishedDate + "<br />"+ response.items[3].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"fifth\" class=\"bimg\" src=" + response.items[4].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[4].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[4].volumeInfo.authors +"</strong><br />" + response.items[4].volumeInfo.publishedDate + "<br />"+ response.items[4].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"sixth\" class=\"bimg\" src=" + response.items[5].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[5].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[5].volumeInfo.authors +"</strong><br />" + response.items[5].volumeInfo.publishedDate + "<br />"+ response.items[5].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"seventh\" class=\"bimg\" src=" + response.items[6].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[6].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[6].volumeInfo.authors +"</strong><br />" + response.items[6].volumeInfo.publishedDate + "<br />"+ response.items[6].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"eighth\" class=\"bimg\" src=" + response.items[7].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[7].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[7].volumeInfo.authors +"</strong><br />" + response.items[7].volumeInfo.publishedDate + "<br />"+ response.items[7].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"ninth\" class=\"bimg\" src=" + response.items[8].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[8].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[8].volumeInfo.authors +"</strong><br />" + response.items[8].volumeInfo.publishedDate + "<br />"+ response.items[8].volumeInfo.description +"</p>");
+        
+        $("#im").append("<a href=\" \"><img id=\"tenth\" class=\"bimg\" src=" + response.items[9].volumeInfo.imageLinks.smallThumbnail + "></a>" + "<h2>" +response.items[9].volumeInfo.title + "    </h2>" + "<p><strong>Authors: "+ response.items[9].volumeInfo.authors +"</strong><br />" + response.items[9].volumeInfo.publishedDate + "<br />"+ response.items[9].volumeInfo.description +"</p>");
         
         
         
@@ -34,7 +43,7 @@ $.ajax({
             });
         
         },
-    });
+    });*/
 
 
 
